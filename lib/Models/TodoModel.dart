537a25e5482f4624d58d0ba03eac6ddb88494ada
DEVI2899 <amirthadevicse@gmail.com>
2024-03-04 12:49:44 +0530

    add

--- lib/Models/TodoModel.dart
+++ lib/Models/TodoModel.dart
@@ -7,18 +7,5 @@ class TodoModel{
 
   TodoModel({required this.id, required this.task,required this.description, required this.addTag});
 
-  // TodoModel copyWith({
-  //   String? id,
-  //   String? task,
-  //   String? description,
-  //   String? addTag,
-  // }) {
-  //   return TodoModel(
-  //     id: id ?? this.id,
-  //     task: task?? this.task,
-  //     description: description ?? this.description,
-  //     addTag: addTag ?? this.addTag,
-  //
-  //   );
-  //}
+
 }
